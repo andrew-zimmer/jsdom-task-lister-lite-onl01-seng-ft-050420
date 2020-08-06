a3e565fbf45b8b1f5f50ff0d48773bcdcd0c9c75
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-08-06 12:18:08 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+const submit = document.querySelector("div#main-content form#create-task-form")[1]
+const input = document.querySelector("div#main-content form#create-task-form")[0]
+const todoList = document.getElementById("tasks")
+
+submit.addEventListener("click", function(event){
+    event.preventDefault()
+    if (input.value){
+        todoList.innerHTML += `<li> ${input.value} </li>`
+        input.value = ''
+    }
+
+})
